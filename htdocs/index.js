b8782bfc3936771cdd17a83a55de28861e486780
galaxykoo <jeffreyheijlen@leerling.wico.be>
2022-05-31 15:08:52 +0200

    Pokemon

---
+++ htdocs/index.js
@@ -0,0 +1,12 @@
+function Pikachu() {
+  window.open("https://www.fanpop.com/clubs/pikachu");
+  //window.location.href = "https://www.fanpop.com/clubs/pikachu";
+}
+function Charizard() {
+  window.open("https://www.fanpop.com/clubs/charizard");
+  //window.location.href = "https://www.fanpop.com/clubs/pikachu";
+}
+function Snorlax() {
+  window.open("https://www.fanpop.com/clubs/snorlax");
+  //window.location.href = "https://www.fanpop.com/clubs/pikachu";
+}
